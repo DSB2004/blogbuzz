acb19c0efdcec0bc84b4434b35b60790b8d5ffc4
Damanjeet Singh <damanjeetsingh434@gmail.com>
2024-09-25 00:48:58 +0530

    database bug fixed

--- src/util/handleDatabase.ts
+++ src/util/handleDatabase.ts
@@ -1,40 +1,34 @@
-import { PoolConnection, Pool } from 'mysql';
+import { Pool, PoolConnection } from 'mysql2/promise'; 
 
-
-export const getConnection = (pool: Pool): Promise<PoolConnection> => {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, connection) => {
-            if (err) {
-                console.log(err)
-                reject(`CONNECTION_ERROR ${err}`);
-            } else {
-                resolve(connection);
-            }
-        });
-    });
+export const getConnection = async (pool: Pool): Promise<PoolConnection> => {
+    try {
+        const connection = await pool.getConnection();
+        return connection;
+    } catch (err) {
+        console.error(err);
+        throw new Error(`CONNECTION_ERROR ${err}`);
+    }
 };
 
-export const executeQuery = (connection: PoolConnection, SQL: string, values?: any[]): Promise<any> => {
-    return new Promise((resolve, reject) => {
-        connection.query(SQL, values, (err, result) => {
-            if (err) {
-                switch (err.code) {
-                    case 'ER_DUP_ENTRY':
-                        reject(new Error('ERR_DUPLICATE_VALUE'));
-                        break;
-                    case 'ER_BAD_FIELD_ERROR':
-                        reject(new Error('ERR_SQL_QUERY'))
-                    case 'PROTOCOL_CONNECTION_LOST':
-                    case 'ECONNREFUSED':
-                    case 'ER_CON_COUNT_ERROR':
-                        reject(new Error("ERR_DATABASE_CONNECTION"));
-                        break;
-                    default:
-                        reject(new Error(err.code));
-                }
-            } else {
-                resolve(result);
-            }
-        });
-    });
+export const executeQuery = async (connection: PoolConnection, SQL: string, values?: any[]): Promise<any> => {
+    try {
+        const [result] = await connection.query(SQL, values);
+        return result;
+    } catch (err) {
+
+        // @ts-ignore
+        switch (err.code) {
+            case 'ER_DUP_ENTRY':
+                throw new Error('ERR_DUPLICATE_VALUE');
+            case 'ER_BAD_FIELD_ERROR':
+                throw new Error('ERR_SQL_QUERY');
+            case 'PROTOCOL_CONNECTION_LOST':
+            case 'ECONNREFUSED':
+            case 'ER_CON_COUNT_ERROR':
+                throw new Error('ERR_DATABASE_CONNECTION');
+            default:
+                    // @ts-ignore
+                throw new Error(err.code);
+        }
+    }
 };
