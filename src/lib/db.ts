788acb3e24cd7ef613dfe68269b47e25ae510856
Damanjeet Singh <damanjeetsingh434@gmail.com>
2024-09-24 23:04:55 +0530

    database bug fixed

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,30 +1,26 @@
-import mysql from "mysql"
-import { DB_DATABASE, DB_HOST, DB_USERNAME, DB_PASSWORD } from "@/env"
-
-const DATABASE_INSTANCE = mysql.createPool(
-    {
-        user: DB_USERNAME,
-        database: DB_DATABASE,
-        password: DB_PASSWORD,
-        host: DB_HOST,
-
-        ssl: {
-            rejectUnauthorized: true
-        }
-    })
+import mysql from "mysql";
+import { DB_DATABASE, DB_HOST, DB_USERNAME, DB_PASSWORD } from "@/env";
 
 
-DATABASE_INSTANCE.on('acquire', () =>
-    console.log("Database Connected")
-)
 
-DATABASE_INSTANCE.on('error', (err) =>
-    console.log("Database error", err)
-)
 
-DATABASE_INSTANCE.on('release', (err) =>
-    console.log("Database Disconnected")
-)
+const DATABASE_INSTANCE = mysql.createPool({
+  user: DB_USERNAME,
+  database: DB_DATABASE,
+  password: DB_PASSWORD,
+  host: DB_HOST,
+  connectionLimit: 10,
+  waitForConnections: true,
+  queueLimit: 0,  
+  ssl: {
+    rejectUnauthorized:true
+    // ca: caCertificate ? Buffer.from(caCertificate, 'base64').toString('utf-8') : undefined,
+  },
+});
 
+
+DATABASE_INSTANCE.on("acquire", () => console.log("Database Connection Acquired"));
+DATABASE_INSTANCE.on("error", (err) => console.error("Database error", err));
+DATABASE_INSTANCE.on("release", () => console.log("Database Connection Released"));
 
 export default DATABASE_INSTANCE;
